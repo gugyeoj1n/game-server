46d24182f80ba092e833ebd154d60d6bd0906da4
gugyeoj1n <kwooj2788@kookmin.ac.kr>
2023-04-07 15:59:34 +0900

    build client & server

---
+++ codes/socket-cpp/tcp-client/tcp-client.cpp
@@ -0,0 +1,62 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <iostream>
+#include <WinSock2.h>
+#include <thread>
+#define PACKET_SIZE 1024
+using namespace std;
+
+SOCKET sock;
+
+void proc_recv();
+
+int main() {
+	WSADATA wsa;
+	WSAStartup(MAKEWORD(2, 2), &wsa);
+	// ----------------------------------------------------
+
+	sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	SOCKADDR_IN addr = {};
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(5000);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	while (1) {
+		if (!connect(sock, (SOCKADDR*)&addr, sizeof(addr)))
+			break;
+	}
+
+	thread proc_c(proc_recv);
+	char buffer[PACKET_SIZE] = { 0 };
+
+	// MESSAGE SEND
+	while (!WSAGetLastError()) {
+		cin >> buffer;
+		send(sock, buffer, strlen(buffer), 0);
+	}
+
+	proc_c.join();
+
+	closesocket(sock);
+
+	// ----------------------------------------------------
+	WSACleanup();
+}
+
+void proc_recv() {
+	char data[PACKET_SIZE] = {};
+	string cmd;
+
+	while (!WSAGetLastError()) {
+		ZeroMemory(&data, PACKET_SIZE);
+		recv(sock, data, PACKET_SIZE, 0);
+		cmd = data;
+
+		if (cmd == "exit")
+			break;
+
+		cout << "FROM SERVER : " << data << endl;
+	}
+}
---
+++ codes/socket-cpp/tcp-server/tcp-server.cpp
@@ -0,0 +1,59 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+
+#include <iostream>
+#include <winsock2.h>
+#include <thread>
+#define PACKET_SIZE 1024
+using namespace std;
+
+SOCKET sock, client_sock;
+
+void proc_recvs();
+
+int main() {
+	WSADATA wsa;
+	WSAStartup(MAKEWORD(2, 2), &wsa);
+	// ----------------------------------------------------
+
+	sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	SOCKADDR_IN addr = {};
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(5000);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	bind(sock, (SOCKADDR*)&addr, sizeof(addr));
+	listen(sock, SOMAXCONN);
+
+	SOCKADDR_IN client = {};
+	int client_size = sizeof(client);
+	ZeroMemory(&client, client_size);
+	client_sock = accept(sock, (SOCKADDR*)&client, &client_size);
+
+	char buffer[PACKET_SIZE] = { 0 };
+	thread proc_s(proc_recvs);
+
+	while (!WSAGetLastError()) {
+		cin >> buffer;
+		send(client_sock, buffer, strlen(buffer), 0);
+	}
+
+	proc_s.join();
+
+	closesocket(client_sock);
+	closesocket(sock);
+
+	// ----------------------------------------------------
+	WSACleanup();
+}
+
+void proc_recvs() {
+	char data[PACKET_SIZE] = {};
+
+	while (!WSAGetLastError()) {
+		ZeroMemory(&data, PACKET_SIZE);
+		recv(sock, data, PACKET_SIZE, 0);
+
+		cout << "FROM CLIENT : " << data << endl;
+	}
+}
