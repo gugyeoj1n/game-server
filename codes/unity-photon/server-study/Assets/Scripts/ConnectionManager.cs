40c0cadf08d459925b14fefdf425cf504f3f68eb
gugyeoj1n <kwooj2788@kookmin.ac.kr>
2023-04-13 16:38:05 +0900

    add scripts folder

---
+++ codes/unity-photon/server-study/Assets/Scripts/ConnectionManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class ConnectionManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Connect() => PhotonNetwork.ConnectUsingSettings();
+}
